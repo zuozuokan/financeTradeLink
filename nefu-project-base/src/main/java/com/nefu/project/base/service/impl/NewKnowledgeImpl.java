c2ab8ce2c3fc4ac9240e263df5162cd9b0a677a4
zuozuokan <14971069+zuozuokan@user.noreply.gitee.com>
2025-06-30 14:10:23 +0800

    修复了一些bug

---
+++ nefu-project-base/src/main/java/com/nefu/project/base/service/impl/NewKnowledgeImpl.java
@@ -0,0 +1,33 @@
+package com.nefu.project.base.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.nefu.project.base.mapper.INewKnowledgeMapper;
+import com.nefu.project.base.service.INewKnowledgeService;
+import com.nefu.project.domain.entity.Knowledge;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class NewKnowledgeImpl implements INewKnowledgeService {
+
+    @Autowired
+    private INewKnowledgeMapper iNewKnowledgeMapper;
+
+    @Override
+    public boolean updateKnowledage(String uuid, Knowledge knowledge) {
+        LambdaQueryWrapper<Knowledge> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+        lambdaQueryWrapper.eq(Knowledge::getKnowledgeUuid, uuid);
+        Knowledge queryKnowledge = iNewKnowledgeMapper.selectOne(lambdaQueryWrapper);
+        queryKnowledge.setKnowledgeTitle(knowledge.getKnowledgeTitle());
+        queryKnowledge.setKnowledgeContent(knowledge.getKnowledgeContent());
+        queryKnowledge.setKnowledgeCategory(knowledge.getKnowledgeCategory());
+        queryKnowledge.setKnowledgeStatus(knowledge.getKnowledgeStatus());
+        int update = iNewKnowledgeMapper.update(queryKnowledge, lambdaQueryWrapper);
+        if (update > 0) {
+            return true;
+        }
+        return false;
+    }
+}
